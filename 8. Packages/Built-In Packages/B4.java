1fb2cd7dc7752d1601abf7c0fb72744ec1636241
Geetesh011 <geeteshpenugonda007@gmail.com>
2025-04-03 20:23:05 +0530

    Delete 8. Packages/Built-In Packages/B4.java

--- 8. Packages/Built-In Packages/B4.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDate;
-
-public class B4 {
-    public static void main(String[] args) {
-        LocalDate today = LocalDate.now();
-        System.out.println("Today's date: " + today);
-    }
-}
