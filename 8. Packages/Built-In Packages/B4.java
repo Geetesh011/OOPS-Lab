04197644113bf60aa6ba02acc767ae1b9722433b
Geetesh011 <geeteshpenugonda007@gmail.com>
2025-04-03 18:56:31 +0530

    Add files via upload

---
+++ 8. Packages/Built-In Packages/B4.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class B4 {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println("Today's date: " + today);
+    }
+}
