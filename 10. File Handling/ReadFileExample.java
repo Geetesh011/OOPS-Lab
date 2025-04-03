9a6c79bc694fbdaf0a8bb6158200b2dcff8b92c7
Geetesh011 <geeteshpenugonda007@gmail.com>
2025-04-03 18:45:09 +0530

    Add files via upload

---
+++ 10. File Handling/ReadFileExample.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+class ReadFileExample {
+    public static void main(String args[]) {
+        try {
+            File myFile = new File("example.txt");
+            Scanner reader = new Scanner(myFile);
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
+                System.out.println(data);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            e.printStackTrace();
+        }
+    }
+}
