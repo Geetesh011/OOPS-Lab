17a843f8b1c6fb3305b553f7aa1780c7eb7f42e4
Geetesh011 <geeteshpenugonda007@gmail.com>
2025-04-03 20:23:16 +0530

    Delete 8. Packages/Built-In Packages/B3.java

--- 8. Packages/Built-In Packages/B3.java
+++
@@ -1,6 +0,0 @@
-public class B3 {
-    public static void main(String[] args) {
-        String message = "Hello, World!";
-        System.out.println("Uppercase: " + message.toUpperCase());
-    }
-}
