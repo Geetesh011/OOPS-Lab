403212c946077346ff0ac01edcc9621195babae9
Geetesh011 <geeteshpenugonda007@gmail.com>
2025-04-03 18:42:22 +0530

    Delete 9. Exception File Handling directory

--- 9. Exception File Handling/TryCatchExample.java
+++
@@ -1,9 +0,0 @@
-class TryCatchExample {
-    public static void main(String args[]) {
-        try {
-            int data = 50 / 0; // ArithmeticException
-        } catch (ArithmeticException e) {
-            System.out.println("Cannot divide by zero!");
-        }
-    }
-}
