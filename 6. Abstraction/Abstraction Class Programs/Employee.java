427cae9a63c2ef55c89410f1c7d20d408acd5dfa
Geetesh011 <geeteshpenugonda007@gmail.com>
2025-04-04 20:45:10 +0530

    Update Employee.java

--- 6. Abstraction/Abstraction Class Programs/Employee.java
+++ 6. Abstraction/Abstraction Class Programs/Employee.java
@@ -1,12 +1,14 @@
 abstract class Employee {
     abstract void showDetails();
 }
+
 class Manager extends Employee {
     void showDetails() {
         System.out.println("Manager Details");
     }
 }
-class EmployeeDemo {
+
+public class EmployeeDemo {
     public static void main(String args[]) {
         Employee obj = new Manager();
         obj.showDetails();
