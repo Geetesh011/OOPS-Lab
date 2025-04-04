5904c9fe0d931d585aebd556198ef44447c5d2c4
Geetesh011 <geeteshpenugonda007@gmail.com>
2025-04-04 20:45:39 +0530

    Update Shape.java

--- 6. Abstraction/Abstraction Class Programs/Shape.java
+++ 6. Abstraction/Abstraction Class Programs/Shape.java
@@ -1,12 +1,14 @@
 abstract class Shape {
     abstract void draw();
 }
+
 class Circle extends Shape {
     void draw() {
         System.out.println("Drawing Circle");
     }
 }
-class ShapeDemo {
+
+public class ShapeDemo {
     public static void main(String args[]) {
         Shape obj = new Circle();
         obj.draw();
