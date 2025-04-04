adb5083cf2e71f41fc4397103167a8292e0c6af4
Geetesh011 <geeteshpenugonda007@gmail.com>
2025-04-04 20:43:11 +0530

    Update MultipleInheritanceExample.java

--- 6. Abstraction/Interface Programs/MultipleInheritanceExample.java
+++ 6. Abstraction/Interface Programs/MultipleInheritanceExample.java
@@ -1,21 +1,28 @@
 interface A {
     void methodA();
 }
+
 interface B {
     void methodB();
 }
+
 class MultipleInheritanceExample implements A, B {
     public void methodA() {
         System.out.println("Method A");
     }
+
     public void methodB() {
         System.out.println("Method B");
     }
 }
-class MultipleInheritanceDemo {
+
+public class MultipleInheritanceDemo {
     public static void main(String args[]) {
-        MultipleInheritanceExample obj = new MultipleInheritanceExample();
-        obj.methodA();
-        obj.methodB();
+        A objA = new MultipleInheritanceExample();
+        objA.methodA();
+        
+        B objB = new MultipleInheritanceExample();
+        objB.methodB();
     }
 }
+
