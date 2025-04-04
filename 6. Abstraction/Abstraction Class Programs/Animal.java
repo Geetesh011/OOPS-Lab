c6456e03206dd7485600af891ed90fe4183b9c6c
Geetesh011 <geeteshpenugonda007@gmail.com>
2025-04-04 20:44:40 +0530

    Update Animal.java

--- 6. Abstraction/Abstraction Class Programs/Animal.java
+++ 6. Abstraction/Abstraction Class Programs/Animal.java
@@ -1,12 +1,14 @@
 abstract class Animal {
     abstract void makeSound();
 }
+
 class Dog extends Animal {
     void makeSound() {
         System.out.println("Barking");
     }
 }
-class AnimalDemo {
+
+public class AnimalSound {
     public static void main(String args[]) {
         Animal obj = new Dog();
         obj.makeSound();
