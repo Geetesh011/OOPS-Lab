c0ca0bf49e36d81f99943c5d4b8c9c6f1a8a9c1c
Geetesh011 <geeteshpenugonda007@gmail.com>
2025-04-04 20:42:40 +0530

    Update Bank.java

--- 6. Abstraction/Interface Programs/Bank.java
+++ 6. Abstraction/Interface Programs/Bank.java
@@ -1,14 +1,17 @@
 interface Bank {
     float getRateOfInterest();
 }
+
 class SBI implements Bank {
     public float getRateOfInterest() {
         return 5.5f;
     }
 }
-class BankDemo {
+
+public class BankDemo {
     public static void main(String args[]) {
-        Bank obj = new SBI();
-        System.out.println("Rate of Interest: " + obj.getRateOfInterest());
+        Bank obj = new SBI(); // Polymorphism: Interface reference holding SBI object
+        System.out.println("Rate of Interest: " + obj.getRateOfInterest() + "%");
     }
 }
+
